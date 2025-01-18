bb1eb1b509690b613505613a1eddb267997050d3
geekcrea <webcrea.a@gmail.com>
2025-01-18 17:20:46 +0100

    error page integration

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import errorImg from '../assets/images/404.png';
+import { Link } from 'react-router-dom';
+import '../../style/ErrorPage.scss';
+export const ErrorPage = () => {
+	return (
+		<div className="error-container">
+			<img src={errorImg} alt="Erreur 404" />
+			<p>Oups! La page que vous demandez n'existe pas </p>
+			<Link to={'/'}>Retournez sur la page d'accueil</Link>
+		</div>
+	);
+};
